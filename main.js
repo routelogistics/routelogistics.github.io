8b721101ef32f1d0e76bc6c8345562888f1c6b34
routelogistics <miraclemicheal220@gmail.com>
2023-08-28 18:00:48 -0700

    xdd

--- main.js
+++ main.js
@@ -8,27 +8,44 @@ const logIn = document.getElementById("logIn");
 const msg = document.getElementById("error");
 const logi = document.getElementById("logi");
 const logNone = document.getElementById("logNone");
+const hide = document.getElementById("hide");
 
 pass.addEventListener("input", () => {
-    if (pass.value.length > 0) {
-        msg.style.display = "block";
+
+    if (pass.value.length > 8) {
+        logi.style.display = "block";
+        logNone.style.display = "none";
+        
     }
-    else {
-        error.style.display = "none";
+    else{
+        logi.style.display = "none";
+        logNone.style.display = "block";
     }
-    if (pass.value.length < 4) {
-        error.textContent = "password is weak";
-        error.style.color ="red";
-    } else
-        if (pass.value.length > 4 && pass.value.length < 6) {
-            error.textContent = "password is medium";
-            error.style.color ="yellow";
-        }
-        else if (pass.value.length > 8) {
-            error.textContent = "password is strong";
-            logi.style.display = "block";
-            logNone.style.display = "none";
-            error.style.color ="green";
-        }
+})
        
-});
+       
+
+hide.addEventListener("click",()=>{
+    if(pass.type === "password"){
+        pass.type="text";
+    }else{
+        pass.type="passowrd";
+    }
+})
+
+
+
+// if (pass.value.length < 4) {
+//     error.textContent = "password is weak";
+//     error.style.color ="red";
+// } else
+//     if (pass.value.length > 4 && pass.value.length < 6) {
+//         error.textContent = "password is medium";
+//         error.style.color ="yellow";
+//     }
+//     else if (pass.value.length > 8) {
+//         error.textContent = "password is strong";
+//         logi.style.display = "block";
+//         logNone.style.display = "none";
+//         error.style.color ="green";
+//     }
